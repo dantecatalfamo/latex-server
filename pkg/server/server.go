06faddb06debacecb1d7f843387c0fdec8bb420d
dantecatalfamo <dante.catalfamo@gmail.com>
2024-01-23 01:13:34 -0500

    Add logger middleware

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func RunServer(config Config) error {
 	mux := chi.NewMux()
+	mux.Use(middleware.Logger)
 	SetupRoutes(config, mux)
 	return http.ListenAndServe(config.ListenAddress, mux)
 }
