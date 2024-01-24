16ebc4b89c965f04057a41090f50bfafad5e2eb4
dantecatalfamo <dante.catalfamo@gmail.com>
2024-01-24 16:27:24 -0500

    Add first step towards auth middleware

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,7 +13,35 @@ import (
 // RunServer starts a server using the given configuration and listens
 func RunServer(config Config) error {
 	mux := chi.NewMux()
+	mux.Use(middleware.RealIP)
 	mux.Use(middleware.Logger)
+	mux.Use(TokenAuthMiddleware(config))
 	SetupRoutes(config, mux)
 	return http.ListenAndServe(config.ListenAddress, mux)
+}
+
+const ContextAuthedUserKey = "authedUser"
+
+func TokenAuthMiddleware(config Config) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var authedUser string
+			authHeader := r.Header.Get("Authorization")
+			if authHeader != "" {
+				split := strings.Split(authHeader, " ")
+				if len(split) > 1 && split[0] == "Bearer" {
+					token := split[1]
+					user, err := GetUserFromToken(config, token)
+					if err != nil {
+						log.Printf("Bad auth token \"%s\": %s", token, err)
+					} else {
+						authedUser = user
+					}
+				}
+			}
+			ctx := context.WithValue(r.Context(), ContextAuthedUserKey, authedUser)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+
+	}
 }
