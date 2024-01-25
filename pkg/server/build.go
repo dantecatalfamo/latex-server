b702424e48c71df6a75ba0e7a6e0f0943335bedd
dantecatalfamo <dante.catalfamo@gmail.com>
2024-01-25 11:18:08 -0500

    Move RunBuildNative to its own file

--- pkg/server/build.go
+++ pkg/server/build.go
@@ -1,13 +1,9 @@
 package server
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
 )
 
 type Engine string
@@ -42,69 +38,4 @@ func RunBuild(ctx context.Context, options BuildOptions) (string, error) {
 		return "", errors.New("docker build mode not yet implemented")
 	}
 	return "", fmt.Errorf("invalid build mode \"%s\"", options.BuildMode)
-}
-
-func RunBuildNative(ctx context.Context, options BuildOptions) (string, error) {
-	var engineArg string
-	switch options.Engine {
-	case EnginePDF:
-		engineArg = "-pdf"
-	case EngineLua:
-		if options.AllowLuaTex {
-			engineArg = "-pdflua"
-		} else {
-			engineArg = "-pdf"
-		}
-	case EngineXeTeX:
-		engineArg = "-pdfxe"
-	default:
-		engineArg = "-pdf"
-	}
-
-	auxDir := fmt.Sprintf("-auxdir=%s", options.AuxDir)
-	outDir := fmt.Sprintf("-outdir=%s", options.OutDir)
-
-	args := []string{engineArg, auxDir, outDir};
-
-	if !options.AllowLatexmkrc {
-		args = append(args, "-norc")
-	}
-
-	if options.Document != "" {
-		args = append(args, options.Document)
-	}
-
-	if options.Force {
-		args = append(args, "-f", "-interaction=nonstopmode")
-	} else {
-		args = append(args, "-interaction=batchmode")
-	}
-
-	if options.FileLineError {
-		args = append(args, "-file-line-error")
-	}
-
-	if options.Dependents {
-		args = append(args, "-deps")
-	}
-
-	err := os.Chdir(options.SrcDir)
-	if err != nil {
-		return "", fmt.Errorf("RunBuild: %w", err)
-	}
-
-	cmd := exec.CommandContext(ctx, "latexmk", args...)
-
-	cmdOut := new(bytes.Buffer)
-	cmd.Stdout = cmdOut
-	cmd.Stderr = cmdOut
-
-	log.Printf("Starting build in %s: %v", options.SrcDir, args)
-	if err := cmd.Run(); err != nil {
-		// If error is type *ExitError, the cmdOut should be populated
-		// with an error message
-		return cmdOut.String(), fmt.Errorf("RunBuild: %w", err)
-	}
-
-	return cmdOut.String(), nil
 }
---
+++ pkg/server/build_native.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func RunBuildNative(ctx context.Context, options BuildOptions) (string, error) {
+	var engineArg string
+	switch options.Engine {
+	case EnginePDF:
+		engineArg = "-pdf"
+	case EngineLua:
+		if options.AllowLuaTex {
+			engineArg = "-pdflua"
+		} else {
+			engineArg = "-pdf"
+		}
+	case EngineXeTeX:
+		engineArg = "-pdfxe"
+	default:
+		engineArg = "-pdf"
+	}
+
+	auxDir := fmt.Sprintf("-auxdir=%s", options.AuxDir)
+	outDir := fmt.Sprintf("-outdir=%s", options.OutDir)
+
+	args := []string{engineArg, auxDir, outDir};
+
+	if !options.AllowLatexmkrc {
+		args = append(args, "-norc")
+	}
+
+	if options.Document != "" {
+		args = append(args, options.Document)
+	}
+
+	if options.Force {
+		args = append(args, "-f", "-interaction=nonstopmode")
+	} else {
+		args = append(args, "-interaction=batchmode")
+	}
+
+	if options.FileLineError {
+		args = append(args, "-file-line-error")
+	}
+
+	if options.Dependents {
+		args = append(args, "-deps")
+	}
+
+	err := os.Chdir(options.SrcDir)
+	if err != nil {
+		return "", fmt.Errorf("RunBuild: %w", err)
+	}
+
+	cmd := exec.CommandContext(ctx, "latexmk", args...)
+
+	cmdOut := new(bytes.Buffer)
+	cmd.Stdout = cmdOut
+	cmd.Stderr = cmdOut
+
+	log.Printf("Starting build in %s: %v", options.SrcDir, args)
+	if err := cmd.Run(); err != nil {
+		// If error is type *ExitError, the cmdOut should be populated
+		// with an error message
+		return cmdOut.String(), fmt.Errorf("RunBuild: %w", err)
+	}
+
+	return cmdOut.String(), nil
+}
